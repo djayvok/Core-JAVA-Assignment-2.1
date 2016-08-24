a02c9f92a88a254f8d627a0b6052bc82890c7e6b
Dhananjaya <djay.vok@gmail.com>
2016-08-24 10:13:42 +0100

    Update and rename .gitignore to PrintWithoutSemicolon.java

---
+++ PrintWithoutSemicolon.java
@@ -0,0 +1,11 @@
+public class PrintWithoutSemicolon
+{
+     public static void main(String[] args) 
+     {
+        int i = 0;
+        for(i = 1; i < 2; System.out.println("Dhananjaya"))
+        {
+            i++;
+     	}
+     }
+}
